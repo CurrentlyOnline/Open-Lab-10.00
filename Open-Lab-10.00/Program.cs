07bf4061790f9b922d403072371aeb09d023a3d7
CurrentlyOnline <sambor433@gmail.com>
2021-11-27 19:44:04 +0100

    Open-Lab-10.02

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -4,17 +4,58 @@ namespace Open_Lab_10._00
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             Book LOTR = new Book();
-            LOTR.title = "LOTR";
-            Console.WriteLine(LOTR.title);
+            LOTR.Title("Lord of the rings");
+            LOTR.Author("Tolkien");
+            LOTR.Category("Fantasy");
+            LOTR.Pages(304);
+            LOTR.RealeaseDate(2002);
+            LOTR.returning();
         }
     }
     class Book
     {
-        public string title;
+        private string title;
         private int pages;
+        private string category;
+        private string author;
+        private double releaseDate;
+
+        public void Title(string setTitle)
+        {
+           title = setTitle;
+        }
+        public void Pages(int setPages)
+        {
+            pages = setPages;
+        }
+        public void Category(string setCategory)
+        {
+           category = setCategory;
+        }
+        public void Author(string setAuthor)
+        {
+           author = setAuthor;
+        }
+        public void RealeaseDate(int setreleaseDate)
+        {
+            releaseDate = setreleaseDate;
+        }
+
+        public void returning()
+        {
+            Console.WriteLine(title);
+            Console.WriteLine(pages);
+            Console.WriteLine(category);
+            Console.WriteLine(author);
+            Console.WriteLine(releaseDate);
+        }
         
     }
 }
+                    
+
+    
+
